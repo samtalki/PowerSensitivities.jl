28e9ebce1a9727a66e8fa6050c43163d941efa38
samtalki <saimouer@gmail.com>
2021-08-13 13:17:22 -0400

    Completed make_sobol_sens_matrix()

--- src/sobol_sensitivties.jl
+++ src/sobol_sensitivties.jl
@@ -1,14 +1,34 @@
 using GlobalSensitivity
 using QuasiMonteCarlo
-using PowerSystems
+include("injections.jl")
 
-function sobol_sens_matrix(model::System,n_order,n_boot,conf_int=0.95)
-    """Construncts an estimate for the global sensitivity matrix of the model"""
-    sampler = SobolSample()
-    A,B = QuasiMonteCarlo.generate_design_matrices(n,lb,ub,sampler)
-    res = gsa(get_voltages,Sobol(order=[0,1,2]),A,B)
+function set_system_model(system_model::System)
+    """Sets the global system model parameters"""
+    if(system_model !== nothing)
+        global system_model = model
+    end
+end
+
+function make_sobol_sens_matrix(param_range::Vector,f::Function;
+    order::Vector=[0,1,2],n_boot::Int64=100,conf_int::Float64=0.95)
+    """
+    Construncts an estimate for the global sensitivity matrix of the model
+        Parameters:
+        ---
+        param_range::Vector ∈ R^N with injections ranges of real or reactive power for buses i=1,…,N
+        f::Function Gets the voltage magnitudes for a given injection at bus i
+        
+    """
+    if (system_model === nothing)
+        println("No global system model, first call set_system_model(system_model::System)")
+    end
+    method = Sobol{
+        order:order,
+        nboot:n_boot,
+        conf_int:conf_int
+    }
+    res = gsa(f,method,param_range; N, batch=false)
     return res
-
 end
 
 function make_vmag_sens_matrix()
@@ -16,10 +36,13 @@ function make_vmag_sens_matrix()
 end
 
 function make_sens_matrix()
+
 end
 
 function make_design_matrices(n,inj_min::Vector,inj_max::Vector)
     A,B = QuasiMonteCarlo.generate_design_matrices(n,inj_min,inj_max,sampler,n)
-
+    return A,B
 end
+
+
 
