b8321db1c9951fbe514a7e2a1a2648831c1b13cc
samtalki <saimouer@gmail.com>
2021-08-13 14:15:02 -0400

    Completed interoperability w/ design matrix and sobol

--- src/injections.jl
+++ src/injections.jl
@@ -3,15 +3,30 @@ using TimeSeries
 using Dates
 using DataFrames
 using LinearAlgebra
-using GlobalSensitivity
-using QuasiMonteCarlo
-#using PowerSensitivities : inj_range_matrix
-#using PowerSensitivities : sensitivity_global
-#include("sensitivities.jl")
 
-function get_voltages(system_model::System=nothing)
-    results = solve_powerflow(system_model)
-    return results["bus_results"][!,"Vm"]
+mutable struct RealPerturb
+    inj_state::Vector
+end
+
+mutable struct ReactivePerturb
+    inj_state::Vector
+end
+
+
+function set_system_model(system_model::System)
+    """Sets the global system model parameters"""
+    if(system_model !== nothing)
+        println("Setting global system model")
+        global system_model = model
+    end
+end
+
+function check_system_model(modelx::System)
+    if(system_model !== nothing) && (modelx==system_model)
+        println("Global System model PASSED")
+    else
+        set_system_model(modelx)
+    end
 end
 
 function get_PQ_buses(sys::System)
@@ -20,11 +35,18 @@ function get_PQ_buses(sys::System)
     return PQ_buses
 end
 
-function get_voltages(bus_results)
+function get_voltages(system_model::System=system_model)
+    results = solve_powerflow(system_model)
+    return results["bus_results"][!,"Vm"]
+end
+
+function get_voltages(bus_results::DataFrame)
     return bus_results["Vm"]
 end
 
-function place_inj(P_inj::Float64,Q_inj::Float64,bus::Bus,name,system_model::System)
+function place_inj(P_inj::Float64,Q_inj::Float64,bus::Bus,name::String;
+    system::System=system_model)
+    check_system_model(system)
     load = PowerLoad(
         name = name,
         available = true,
@@ -45,7 +67,9 @@ function place_inj(P_inj::Float64,Q_inj::Float64,bus::Bus,name,system_model::Sys
     return load
 end
 
-function place_inj(P_inj::Float64,Q_inj::Float64,inj_bus::Int,name::String,system_model::System)
+function place_inj(P_inj::Float64,Q_inj::Float64,inj_bus::Int,name::String;
+    system::System=system_model)
+    check_system_model(system)
     load = PowerLoad(
         name = name,
         available = true,
@@ -64,28 +88,74 @@ function remove_inj(load::PowerLoad,system_model::System)
     remove_component!(system_model,load)
 end
 
+function get_inj_voltages(injection::RealPerturb;system::System=system_model)
+    """
+    Gets a vector of voltage measurements for a real power injection state vector ∈ R^L
+    """
+    check_system_model(system)
+    for (inj_bus,inj) in enumerate(injection.inj_state)
+        if(inj == 0)
+            continue
+        else if inj != 0
+            println("P Inj: ",string(inj),"On bus: ",string(inj_bus))
+            voltages = get_P_inj_voltages(inj_bus,inj,system)
+        end
+    end
+    return voltages
+end
+
+
+function get_inj_voltages(injection::ReactivePerturb;system::System=system_model)
+    """
+    Gets a vector of voltage measurements for a real power injection state vector ∈ R^L
+    """
+    check_system_model(system)
+    for (inj_bus,inj) in enumerate(injection.inj_state)
+        if(inj == 0)
+            continue
+        else if inj != 0
+            println("Q Inj: ",string(inj),"On bus: ",string(inj_bus))
+            voltages = get_Q_inj_voltages(inj_bus,inj,system)
+        end
+    end
+    return voltages
+end
+
+
+function get_P_inj_voltages(inj_bus::Int,P_inj::Int64,system_model::System)
+    """Gets the vector of voltage magnitudes for an injection on bus num inj_bus of value P_inj"""
+    Q_inj = 0
+    load = place_inj(P_inj,Q_inj,inj_bus,"Inj "+string(inj_bus),system=system_model)
+    voltages = get_voltages(system_model)
+    remove_inj(load,system_model)
+    return voltages
+end
+
+
+
+function get_Q_inj_voltages(inj_bus::Int,Q_inj::Int64,system_model::System)
+    """Gets the vector of voltage magnitudes for an injection on bus num inj_bus of value P_inj"""
+    P_inj = 0
+    load = place_inj(P_inj,Q_inj,inj_bus,"Inj "+string(inj_bus),system=system_model)
+    voltages = get_voltages(system_model)
+    remove_inj(load,system_model)
+    return voltages
+end
+
+
+
 function get_inj_range_voltages(inj_bus::Bus,range_P_inj::Vector,range_Q_inj::Vector,system_model)
     """Gets the voltage mags for each measured bus for each value of the injection in P and Q inj_range at inj_bus"""
     voltages_per_inj = []
     for (idx,(P_inj,Q_inj)) in enumerate(zip(range_P_inj,range_Q_inj))
         println(string(idx),typeof(string(idx)))
-        load = place_inj(P_inj,Q_inj,inj_bus,string(idx),system_model)
+        load = place_inj(P_inj,Q_inj,inj_bus,string(idx),system=system_model)
         voltages = get_voltages(system_model)
         remove_inj(load,system_model)
         push!(voltages_per_inj,voltages)
     end
     return voltages_per_inj
 end
-
-function get_P_inj_voltages(inj_bus::Int,P_inj::Int64,system_model::System)
-    """Gets the vector of voltage magnitudes for an injection on bus num inj_bus of value P_inj"""
-    Q_inj = 0
-    load = place_inj(P_inj,Q_inj,inj_bus,"Inj "+string(inj_bus),system_model)
-    voltages = get_voltages(system_model)
-    remove_inj(load,system_model)
-    return voltages
-end
-
 
 function get_P_voltage_matrix(inj_matrix)
     voltage_matrix = zeros(size(inj_matrix))
@@ -110,35 +180,35 @@ function inj_range_matrix(inj_min,inj_max,n_injections)
     #Diagonal([(inj_min,inj_max) for i in 1:n_injections])
 end
 
-function sensitivity_global(inj_range_matrix,f_v=get_inj_range_voltages,method=Sobol(),order=2,N=1000,batch=true,
-                            nboot=100,conf_int=0.95)
-    """Numeric global sensitivities for a single inj_bus"""
-    sens = GlobalSensitivity.gsa(f_v,method,inj_range_matrix; N, batch=batch,order=order,nboot=nboot,conf_int=conf_int)
-    return sens
-end
-
-#function sobol_design_matrices(n)
-
-
 base_dir = PowerSystems.download(PowerSystems.TestData; branch = "master");
 sys = System(joinpath(base_dir, "matpower/case14.m"));
-@time begin
-    vmag_0 = get_voltages(sys);
-    PQ_buses = get_PQ_buses(sys);
-    inj_bus = PQ_buses[1];
-    range_P_inj = [i for i in 0:0.001:1.0];
-    range_Q_inj = [0.0 for i in 0:0.001:1.0];
-    voltages_per_inj = get_inj_range_voltages(inj_bus,range_P_inj,range_Q_inj,sys);
-end
+PQ_buses = get_PQ_buses(sys);
+
+# run_1 = false
+# if run_1
+#     @time begin
+#         vmag_0 = get_voltages(sys);        
+#         inj_bus = PQ_buses[1];
+#         range_P_inj = [i for i in 0:0.001:1.0];
+#         range_Q_inj = [0.0 for i in 0:0.001:1.0];
+#         voltages_per_inj = get_inj_range_voltages(inj_bus,range_P_inj,range_Q_inj,sys);
+#     end
+# end
 
 #GSA Experiment (Sobol Indeces)
 
-P_inj_min = 0.0001
-P_inj_max = 0.5
-println(P_inj_min,P_inj_max)
-P_inj_params = inj_range_matrix(P_inj_min,P_inj_max,length(PQ_buses))
-sens = sensitivity_global(P_inj_params)
-#gsa_results = DataFrame(Inj_Bus = PQ_buses, inj_range = inj_range)
+
+# P_inj_min = [0.1, 0.1, 0.1]
+# P_inj_max = [0.5,0.5,0.5]
+# N=1000
+# sampler = SobolSample()
+# A,B = QuasiMonteCarlo.generate_design_matrices(N,P_inj_min,P_inj_max,sampler)
+
+# println(P_inj_min,P_inj_max)
+# P_inj_params = inj_range_matrix(P_inj_min,P_inj_max,length(PQ_buses))
+# sens = sensitivity_global(P_inj_params)
+# #gsa_results = DataFrame(Inj_Bus = PQ_buses, inj_range = inj_range)
+
 
 
 
--- src/sobol_sensitivties.jl
+++ src/sobol_sensitivties.jl
@@ -1,21 +1,30 @@
+using Base: Bool, Int64
 using GlobalSensitivity
 using QuasiMonteCarlo
 include("injections.jl")
 
-function set_system_model(system_model::System)
-    """Sets the global system model parameters"""
-    if(system_model !== nothing)
-        global system_model = model
+function make_sobol_sens_matrix(inj_range::Tuple,L_nodes::Int64,inj_type::String;
+    N_simulation::Int64=1000,order::Vector=[0,1,2],n_boot::Int64=100,conf_int::Float64=0.95)
+    #Matrix whose rows are a vector with L entries, with only one nonzero element containing tuples for the bus to be measured
+    param_range_matrix = inj_range_matrix(inj_range[0],inj_range[1],L_nodes)
+    for row in eachrow(param_range_matrix)
+        param_range = Vector(row)
+        if(occursin("p",inj_type) || occursin("P",inj_type))
+            results = make_sobol_sens_vector(param_range,get_P_inj_voltages;param_range,get_Q_inj_voltages,N_simulations;order=order,n_boot=n_boot,conf_int=conf_int)
+        else if occursin("q",inj_type) || occursin("Q",inj_type)
+            results = make_sobol_sens_vector(param_range,get_Q_inj_voltages,N_simulations;order=order,n_boot=n_boot,conf_int=conf_int)
+        end
     end
+    return sensitivity_matrix
 end
 
-function make_sobol_sens_matrix(param_range::Vector,f::Function;
+function make_sobol_sens_vector(param_range::Vector,f::Function,N_simulations::Int64;
     order::Vector=[0,1,2],n_boot::Int64=100,conf_int::Float64=0.95)
     """
     Construncts an estimate for the global sensitivity matrix of the model
         Parameters:
         ---
-        param_range::Vector ∈ R^N with injections ranges of real or reactive power for buses i=1,…,N
+        param_range::Vector ∈ R^L with injections ranges of real or reactive power for buses i=1,…,N
         f::Function Gets the voltage magnitudes for a given injection at bus i
         
     """
@@ -27,12 +36,8 @@ function make_sobol_sens_matrix(param_range::Vector,f::Function;
         nboot:n_boot,
         conf_int:conf_int
     }
-    res = gsa(f,method,param_range; N, batch=false)
+    res = gsa(f,method,param_range; N_simulations, batch=false)
     return res
-end
-
-function make_vmag_sens_matrix()
-    
 end
 
 function make_sens_matrix()
@@ -43,6 +48,16 @@ function make_design_matrices(n,inj_min::Vector,inj_max::Vector)
     A,B = QuasiMonteCarlo.generate_design_matrices(n,inj_min,inj_max,sampler,n)
     return A,B
 end
+
+
+
+function sensitivity_global(inj_range_matrix,f_v=get_inj_range_voltages,
+    method=Sobol(),order=2,N=1000,batch=true,nboot=100,conf_int=0.95)
+    """Numeric global sensitivities for a single inj_bus"""
+    sens = GlobalSensitivity.gsa(f_v,method,inj_range_matrix; N, batch=batch,order=order,nboot=nboot,conf_int=conf_int)
+    return sens
+end
+
 
 
 
