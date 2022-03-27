483b59fbd9039bad7f2638f8b87494bc9a2dafe4
samtalki <saimouer@gmail.com>
2022-03-27 17:23:16 -0400

    Update manifest

---
+++ src/sensitivities/sensitivities.jl
@@ -0,0 +1,10 @@
+struct SensitivityMatrix
+    spv::AbstractMatrix
+	sqv::AbstractMatrix
+    spth::AbstractMatrix
+    sqth::AbstractMatrix
+end
+
+function calc_sensitivity_matrix(J::JacboianMatrix)
+    
+end
