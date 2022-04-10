5fe1c9a0018bbea4905d2f7147c58e3bc3fba917
samtalki <saimouer@gmail.com>
2022-04-10 11:06:17 -0400

    Condense into one-line functions

--- src/power_factor.jl
+++ src/power_factor.jl
@@ -5,56 +5,27 @@ calc_basic_power_factor(network::Dict{String,<:Any}) =  [abs(cos(θi)) for θi i
 #calc_basic_power_factor(network::Dict{String,<:Any}) =  cos.(angle.(calc_basic_bus_injection(network)))
 
 """
-Given a real power factor pf return the implicit function theorem representation of pf
+Implicit function theorem representation of reactive power. This is used to make entries of the K matrix.
+    Params:
+    pf: Power factor(s)
+    q: Reactive power injections(s) used for signed representation.
 """
-function k(pf::Real)
-    #@assert pf<= 1 && pf > 0 "Power factors must be between (0,1]"
-    return sqrt(1-pf^2)/pf
-end
+k(pf::Real) =  pf>0 ? sqrt(1-pf^2)/pf : NaN
+k(pf::Real,q::Real) = pf>0 ? -sign(q)*abs(sqrt(1-pf^2)/pf) : NaN #SIGNED implicit representation of reactive power
+k(network::Dict{String,<:Any}) = [k(pf_i) for pf_i in calc_basic_power_factor(network)] #Given a network data dict return a vector of implicit representations of reactive power for all node power factors
+k(network::Dict{String,<:Any},q::Vector) = [k(pf_i,q_i) for (pf_i,q_i) in zip(calc_basic_power_factor(network),q)] #Given a network data dict return a vector of implicit representations for all node power factors
 
 """
-Given a real power factor pf and the reactive power injection return the SIGNED implicit function theorem representation of pf
-This is used to make entries of the K matrix.
+Implicit function theorem representation of reactive power.
 """
-function k(pf::Real,q::Real)
-    #@assert pf<= 1 && pf > 0 "Power factors must be between (0,1]"
-    return -sign(q)*abs(sqrt(1-pf^2)/pf)
-end
-
-"""
-Given a network data dict return a vector of implicit function theorem representations for all node power factors
-Makes entries of the K matrix.
-"""
-function k(network::Dict{String,<:Any})
-    pf = calc_basic_power_factor(network)
-    return [k(pf_i) for pf_i in pf]
-end
-
-"""
-Given a real valued β, compute inverse function of k
-"""
-function kinv(β::Real)
-    #@assert β<= 1 && β > 0 "Arugment of k^{-1} must be between (0,1]"
-    if β<=0
-        return nothing
-    else
-        return sqrt(1/(k(β)^2 + 1))
-    end
-end
-
-"""
-Given a network data dict, compute inverse function of k
-"""
-function kinv(network::Dict{String,<:Any})
-    pf = calc_basic_power_factor(network)
-    return [k_inv(pf_i) for pf_i in pf]
-end
+kinv(β::Real) = β>0 ? sqrt(1/(k(β)^2 + 1)) : NaN
+kinv(network::Dict{String,<:Any}) = [k_inv(pf) for pf in calc_basic_power_factor(network)] #Given a network data dict, compute vector of inverse representations
 
 """
 Compute K matrix where K = diag(√(1-pf_i^2)/pf_i)
 """
 function calc_K_matrix(network::Dict{String,<:Any},ϵ=1e-6)
-    bad_idx = calc_bad_idx(network) #Array of indeces with zero p or zero MVA injections to be discarded
+    bad_idx = calc_bad_idx(network,ϵ) #Array of indeces with zero p or zero MVA injections to be discarded
     pf = calc_basic_power_factor(network) 
     n = length(pf)
     K = zeros((n,n))
@@ -71,20 +42,20 @@ end
 """
 Given network data dict, calculate the maximum or minimum values of the K matrix
 """
-calc_k_max(network::Dict{String,<:Any}) = maximum(diag(calc_K_matrix(network)))
-calc_k_min(network::Dict{String,<:Any}) = minimum(diag(calc_K_matrix(network)))
+calc_k_max(network::Dict{String,<:Any},ϵ=1e-6) = maximum(diag(calc_K_matrix(network,ϵ)))
+calc_k_min(network::Dict{String,<:Any},ϵ=1e-6) = minimum(diag(calc_K_matrix(network,ϵ)))
 
 """
 Given a network data dict, calculate the Δk=k_max-k_min value for the current operating point
 """
 function calc_delta_k(network::Dict{String,<:Any},ϵ=1e-6)
-    K = calc_K_matrix(network)
+    K = calc_K_matrix(network,ϵ)
     Δk = maximum(diag(K)) - minimum(diag(K))
     return Δk
 end
 
 function calc_delta_K_matrix(network::Dict{String,<:Any},ϵ=1e-6)
-    K = calc_K_matrix(network)
+    K = calc_K_matrix(network,ϵ)
     k_max = calc_k_max(network)
     return k_max*I - K
 end
@@ -92,10 +63,10 @@ end
 """
 Given network data dict and sel_bus_types, calculate the M Matrix defined in Theorem 1
 """
-function calc_M_matrix(network::Dict{String,<:Any})
+function calc_M_matrix(network::Dict{String,<:Any},ϵ=1e-6)
+    k_max = calc_k_max(network,ϵ)
     ∂p∂θ = calc_pth_jacobian(network)
     ∂q∂θ = calc_qth_jacobian(network)
-    k_max = calc_k_max(network)
     M = k_max*∂p∂θ - ∂q∂θ
     return M
 end
